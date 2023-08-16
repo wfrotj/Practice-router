7c84bf46bb0b1f205dfae0479cc5603d5fe24452
wfrotj <trebrodrigo@gmail.com>
2023-08-16 22:21:44 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Faq from "./pages/help/Faq";
+import Contact from "./pages/help/Contact";
+import NotFound from "./pages/NotFound";
+// import Careers, { careersLoader } from "./pages/careers/Careers";
+// import CareerDetails, {
+//   careerDetailsLoader,
+// } from "./pages/careers/CareerDetails";
+
+// layouts
+import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
+// import CareersLayout from "./layouts/CareersLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
